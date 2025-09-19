b0d5f8706481b8b52d9b8cd879afcf0b190ce60d
Dan Tasse <dan.tasse@gmail.com>
2025-09-19 10:22:57 -0400

    3x3 grid that you can click and see an x

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,48 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [squares, setSquares] = useState<string[]>(Array(9).fill(''))
+
+  const handleSquareClick = (index: number) => {
+    const newSquares = [...squares]
+    if (newSquares[index] === '') {
+      newSquares[index] = 'X'
+      setSquares(newSquares)
+    }
+  }
+
+  const renderSquare = (index: number) => {
+    return (
+      <button 
+        className="square" 
+        onClick={() => handleSquareClick(index)}
+        key={index}
+      >
+        {squares[index]}
+      </button>
+    )
+  }
 
   return (
-    <>
-      <div>
-      hello world
+    <div className="game">
+      <div className="game-board">
+        <div className="board-row">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
       </div>
-    </>
+    </div>
   )
 }
 
